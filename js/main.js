a1db7a68f589fc865656284cd6c393c89189f529
Mariah Alderson <mariah@Mariahs-MacBook-Pro.local>
2019-01-19 14:05:49 -0500

    added content to landing page, set up mobile styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //hamburger menu open/close
 var hamburger = document.querySelector("#hamburgermenu"),
+    siteheader = document.querySelector("header"),
     sitenav = document.querySelector("#navlist"),
     subnav = document.querySelector(".subnav"),
     arrow = document.querySelector(".downarrow");
@@ -7,6 +8,7 @@ var hamburger = document.querySelector("#hamburgermenu"),
     hamburger.addEventListener("click", function(){
         hamburger.classList.toggle("hamburgeropen");
         sitenav.classList.toggle("showmenu");
+        siteheader.classList.toggle("changecolour");
     });
 
     arrow.addEventListener("click", function(){
