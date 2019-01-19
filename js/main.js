d704de007d403b640df6acefb92a602bcf39eddc
Mariah Alderson <mariah@Mariahs-MacBook-Pro.local>
2019-01-19 15:15:12 -0500

    alt landing style, logo responsive added

--- js/main.js
+++ js/main.js
@@ -3,22 +3,50 @@ var hamburger = document.querySelector("#hamburgermenu"),
     siteheader = document.querySelector("header"),
     sitenav = document.querySelector("#navlist"),
     subnav = document.querySelector(".subnav"),
+    logo = document.querySelector("#logobox img"),
     arrow = document.querySelector(".downarrow");
 
-    hamburger.addEventListener("click", function(){
-        hamburger.classList.toggle("hamburgeropen");
-        sitenav.classList.toggle("showmenu");
+    //change logo from icon to full
+if (matchMedia) {
 
-        //keep header white until nav finishes closing
-        if(siteheader.classList.contains("changecolour")){
-            setTimeout(function(){
-                siteheader.classList.remove("changecolour");
-            },500);
-        }else{
-            siteheader.classList.add("changecolour");
-        }
-    });
+const mq = window.matchMedia("(min-width: 850px)");
+mq.addListener(WidthChange);
+WidthChange(mq);
+}
 
-    arrow.addEventListener("click", function(){
-        subnav.classList.toggle("opensubnav");
-    })
+    // media query change
+function WidthChange(mq) {
+
+    if (mq.matches) {
+        logo.src="images/OK_fullHorizontal.svg";
+    } else {
+        logo.src="images/OK_whiteSymbol.svg";
+    }
+}
+
+    //keep header white until nav finishes closing
+function headerColour(){
+    
+    if(siteheader.classList.contains("changecolour")){
+        setTimeout(function(){
+            siteheader.classList.remove("changecolour");
+        },500);
+    }else{
+        siteheader.classList.add("changecolour");
+    }
+}
+
+
+    //-------EVENT LISTENERS/CLICK FUNCTIONS----------//
+
+hamburger.addEventListener("click", function(){
+    hamburger.classList.toggle("hamburgeropen");
+    sitenav.classList.toggle("showmenu");
+    headerColour();
+        
+});
+
+arrow.addEventListener("click", function(){
+    subnav.classList.toggle("opensubnav");
+    headerColour();
+});
